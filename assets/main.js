dbe52c13f109d7c7a6ff0e7b422b77365f3523fe
e9io <e9io@fake.com>
2018-02-14 00:49:39 +0000

    done

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,35 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/3459063.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log('courses in-progress', response.courses.completed)
+      addCourses(response.courses.completed)
+    }
+  });
 
+  function addCourses(courses) {
+    var badges = $('#badges')
+    courses.forEach(function(course) {
+      var $course = $('<div/>', {
+        'class': 'course'
+      }).appendTo(badges)
+
+      $('<h3/>', {
+        text: course.title
+      }).appendTo($course)
+
+      $('<img/>', {
+        src: course.badge
+      }).appendTo($course)
+
+      $('<a/>', {
+        'class': 'btn btn-primary',
+        href: course.url,
+        target: '_blank',
+        text: 'See Course'
+      }).appendTo($course)
+    })
+  }
 });
